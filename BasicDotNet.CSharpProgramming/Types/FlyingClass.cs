b96e02cf77561eecddcfd363ecff8f9578bab1f4
Luis Felipe Fernandez Marquez <LuisFelipe.Fernandez@mrw.es>
2018-08-07 17:43:56 +0200

    Added some code snippets.

---
+++ BasicDotNet.CSharpProgramming/Types/FlyingClass.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicDotNet.CSharpProgramming
+{
+    public enum FlyingClass
+    {
+        Luxury,     //0
+        Bussiness,  //1
+        Tourist,    //2
+        Economy     //3
+    }
+}
---
+++ BasicDotNet.CSharpProgramming/Types/PaxInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BasicDotNet.CSharpProgramming.Types
+{
+    class PaxInfo
+    {
+        void ProcessInfo()
+        {
+            var e = FlyingClass.Luxury;
+            var stc = new PaxBasicInfo();
+            stc.Name = "John";
+            stc.BirthDate = new DateTime(1,5,1995);
+        }
+    }
+}
