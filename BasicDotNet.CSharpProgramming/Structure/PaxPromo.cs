8f00fcc069cedf94bcf44b645bd726a9afc08c63
Luis Felipe Fernandez Marquez <LuisFelipe.Fernandez@mrw.es>
2018-08-08 08:37:39 +0200

    Added PaxPromo class.

---
+++ BasicDotNet.CSharpProgramming/Structure/PaxPromo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BasicDotNet.CSharpProgramming.Structure
+{
+    class PaxPromo
+    {
+        protected int YoungPromoThreshold { get; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public PaxPromo(int threshold)
+        {
+            YoungPromoThreshold = threshold;
+        }
+        public bool IsYoungPromoCandidate()
+        {
+            var now = DateTime.Now;
+            return now.Subtract(BirthDate).TotalDays / 365 < YoungPromoThreshold;
+        }
+    }
+}
