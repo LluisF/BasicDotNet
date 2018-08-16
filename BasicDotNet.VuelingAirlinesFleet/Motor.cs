1558c5e9f345ad23544ec6e5f8507c989089b232
Luis Felipe Fernandez Marquez <LuisFelipe.Fernandez@mrw.es>
2018-08-16 09:04:56 +0200

    Added VuelingAirlinesFleet

---
+++ BasicDotNet.VuelingAirlinesFleet/Motor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicDotNet.VuelingAirlinesFleet
+{
+    public class Motor
+    {
+        public string Maker {get; private set;}
+        public string Model {get; private set;}
+        public double FuelConsumption {get; private set;}
+        public MotorType Type {get; set;}
+        public MotorState State {get; set;}
+        public void Start() { }
+        public void Stop() { }
+        public void IncreaseImpulsion(){}
+    }
+}
