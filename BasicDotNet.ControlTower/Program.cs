6ed8f1a2d911de87e85b36e31f82b3adcfebd97b
José González <pjosegq90@gmail.com>
2018-08-23 10:01:45 +0200

    i

--- BasicDotNet.ControlTower/Program.cs
+++ BasicDotNet.ControlTower/Program.cs
@@ -6,7 +6,7 @@ namespace BasicDotNet.ControlTower
     {
         static void Main(string[] args)
         {
-            FlightPlan flightPlan = new FlightPlan(new DateTime(2018, 08, 20, 20, 07, 00), new DateTime(2018, 08, 20, 20, 53, 00), "BCN", "MAD", 900);
+            FlightPlan flightPlan = new FlightPlan(new DateTime(2018, 08, 21, 09, 07, 00), new DateTime(2018, 08, 21, 10, 20, 00), "BCN", "MAD", 3000);
             Airplane airplane = new Airplane("Boeing", "737", "Jose", flightPlan);
             Console.WriteLine("Capacidad: " + airplane.Capacity);
             Console.WriteLine("Autonomia: " + airplane.Autonomy);
