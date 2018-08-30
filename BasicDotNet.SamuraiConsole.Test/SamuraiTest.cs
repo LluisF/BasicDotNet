7a9e739686721c01936b402cc21196f983ab38a1
Luis Felipe Fernandez Marquez <LuisFelipe.Fernandez@mrw.es>
2018-08-30 09:00:10 +0200

    Prepared Kata.

--- BasicDotNet.SamuraiConsole.Test/SamuraiTest.cs
+++ BasicDotNet.SamuraiConsole.Test/SamuraiTest.cs
@@ -13,26 +13,17 @@ namespace BasicDotNet.SamuraiConsole.Test
             // Preparo el combate
 
             // Primero las armas...
-            var swordUnderTest = new Sword();
-            var foeSword = new Sword();
-            var swordDamage = swordUnderTest.Damage;
-            var foeDamage = foeSword.Damage;
 
             // ... luego, los contendientes, cada uno con su arma
-            var samuraiUnderTest = new Samurai(swordUnderTest, 100);
-            var foeSamurai = new Samurai(foeSword, 75);
 
             // Examino a los contendientes...
-            var samuraiInitialHealth = samuraiUnderTest.Health;
-            var foeInitialHealth = foeSamurai.Health;
 
             // ... y ¡lanzo el ataque!
-            samuraiUnderTest.Attack(foeSamurai);
 
             // Compruebo que el enemigo se ha hecho pupita...
-            Assert.IsTrue(foeSamurai.Health == foeInitialHealth - swordDamage);
+
             // ...que yo estoy sanito...
-            Assert.IsTrue(samuraiInitialHealth == samuraiUnderTest.Health);
+
         }
     }
 }
--- BasicDotNet.SamuraiConsole.WithMocking.Test/SamuraiTest.cs
+++ BasicDotNet.SamuraiConsole.WithMocking.Test/SamuraiTest.cs
@@ -13,21 +13,14 @@ namespace BasicDotNet.SamuraiConsole.WithMocking.Test
         public void AttackTest()
         {
             // Moqueo las dependencias del samurai...
-            var mockSword = new Mock<IWeapon>();
-            var mockFoeSword = new Mock<IWeapon>();
 
             // Preparo a los samurais...
-            var samuraiUnderTest = new Samurai(mockSword.Object, 100);
-            var foeSamurai = new Samurai(mockFoeSword.Object, 75);
 
             // Grabo las expectativas sobre los mocks...
-            mockSword.Setup(sword => sword.Hit(It.IsAny<IWarrior>()));
 
             // Lanzo la unidad de código a probar..
-            samuraiUnderTest.Attack(foeSamurai);
 
             // Verifico que se ha cumplido el comportamiento esperado...
-            mockSword.VerifyAll();
         }
 
         [TestMethod]
@@ -35,42 +28,14 @@ namespace BasicDotNet.SamuraiConsole.WithMocking.Test
         public void SuicidalAttackTest()
         {
             // Moqueo las dependencias del samurai...
-            var mockSword = new Mock<IWeapon>();
 
             // Preparo al samurai...
-            var samuraiUnderTest = new Samurai(mockSword.Object, 100);
 
             // Grabo las expectativas sobre los mocks...
-            mockSword.Setup(sword => sword.Hit(It.IsAny<IWarrior>())).Throws(It.IsAny<SuicidalException>());
 
             // Lanzo la unidad de código a probar..
-            samuraiUnderTest.Attack(samuraiUnderTest);
 
             // Verifico que se ha cumplido el comportamiento esperado...
-            mockSword.VerifyAll();            
-        }
-
-        private Mock<IWeapon> _looseMock;
-        private Mock<IWeapon> _anotherLooseMock;
-        private Mock<IWeapon> _yetAnotherLooseMock;
-        private Mock<IWeapon> _strictMock;
-
-        [TestInitialize]
-        public void BeforeEachTest()
-        {
-            _looseMock = new Mock<IWeapon>(MockBehavior.Loose);
-            _anotherLooseMock = new Mock<IWeapon>();
-            _yetAnotherLooseMock = new Mock<IWeapon>(MockBehavior.Default);
-            _strictMock = new Mock<IWeapon>(MockBehavior.Strict);
-        }
-
-        [TestMethod]
-        public void Test()
-        {
-            _looseMock.SetupGet(weapon => weapon.Damage).Returns(It.IsAny<int>());
-            _looseMock.Setup(weapon => weapon.Hit(It.IsAny<IWarrior>()));
-            _looseMock.Setup(weapon => weapon.Hit(It.IsAny<IWarrior>())).AtMost(5);
-            _looseMock.Setup(weapon => weapon.Hit(It.IsAny<IWarrior>())).Throws(It.IsAny<SuicidalException>());            
         }
     }
 }
